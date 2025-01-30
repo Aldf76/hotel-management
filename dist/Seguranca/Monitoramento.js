a6621371844e958d6aff88eb497c690d368b3ffb
Aldf76 <contasfelipe623@gmail.com>
2025-01-30 12:21:38 -0300

    commit de correção

---
+++ dist/Seguranca/Monitoramento.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Monitoramento = void 0;
+class Monitoramento {
+    registrarOcorrencia(ocorrencia) {
+        console.log(`Ocorrência registrada: ${ocorrencia}`);
+    }
+}
+exports.Monitoramento = Monitoramento;
